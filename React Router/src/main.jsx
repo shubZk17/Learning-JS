d1247faa14f79a9fdc4089b980686f3473f580ac
Shubham Kumar <shubhamkr17074@gmail.com>
2025-06-04 15:27:34 +0530

    Basic Landing Page using React Router

--- React Router/src/main.jsx
+++ React Router/src/main.jsx
@@ -1,11 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Layout from './components/Layout.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "About",
+        element: <About/>
+      },
+      {
+        path: "Contact",
+        element: <Contact/>
+      }
+    ]
+
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router= {router}/>
   </StrictMode>,
 )
